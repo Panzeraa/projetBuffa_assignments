f892edfd0383a89a354af6581273b919e328721c
Panz <alexpanz06@gmail.com>
2021-03-02 16:15:23 +0100

    list subjects

--- web/src/app/shared/subjects.service.ts
+++ web/src/app/shared/subjects.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LoggingService } from './logging.service';
 import {Observable, of} from 'rxjs';
+import { Subject } from '../subjects/subject.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,13 @@ export class SubjectsService {
   constructor(private logginService: LoggingService,
     private http: HttpClient) { }
 
+  public subjects: Subject;
+
   uri = 'http://localhost:8010/api/subjects';
 
-  getSubjects(): Observable<Object> {
-    return this.http.get<Object>(this.uri)
+  public getSubjects() {
+    this.http.get<Subject>(this.uri).subscribe((data) => {
+      this.subjects = data;
+    })
   }
 }
--- web/src/app/subjects/subjects.component.ts
+++ web/src/app/subjects/subjects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectsService } from '../shared/subjects.service';
 
 @Component({
   selector: 'app-subjects',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public subjectsService: SubjectsService) { }
 
   ngOnInit(): void {
+
+    this.subjectsService.getSubjects()
   }
 
 }
